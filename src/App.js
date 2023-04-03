88e27999638621f0315e489aef31cbe932f212f1
Daniel Dimitorv <115570079+malkotohuski@users.noreply.github.com>
2023-04-03 18:25:32 +0300

    Paldin class info

--- src/App.js
+++ src/App.js
@@ -14,10 +14,16 @@ import Marketing from './components/Pages/Marketing';
 import Consulting from './components/Pages/Consulting';
 import { Login } from './components/Pages/Login'
 import { Footer } from './components/Footer';
-import Paladin from './components/Pages/Paladin/Paladin';
 import { useState, useEffect, } from 'react';
 import * as HeroService from './services/HeroService';
 import { Catalog } from './components/Catalog/Catalog';
+import Warrior from './components/Pages/Warrior/Warrior';
+import Mage from './components/Pages/Mage/Mage';
+import Druid from './components/Pages/Druid/Druid';
+import Paladin from './components/Pages/Paladin/Paladin';
+import Hunter from './components/Pages/Hunter/Hunter';
+import Priest from './components/Pages/Priest/Priest';
+import Rogue from './components/Pages/Rogue/Rogue';
 
 
 
@@ -59,6 +65,12 @@ function App() {
                     <Route path='/marketing' element={<Marketing />} />
                     <Route path='/consulting' element={<Consulting />} />
                     <Route path='/class/paladin' element={<Paladin />} />
+                    <Route path='/class/warrior' element={<Warrior />} />
+                    <Route path='/class/mage' element={<Mage />} />
+                    <Route path='/class/druid' element={<Druid />} />
+                    <Route path='/class/hunter' element={<Hunter />} />
+                    <Route path='/class/priest' element={<Priest />} />
+                    <Route path='/class/rogue' element={<Rogue />} />
                     <Route path='/catalog' element={<Catalog hero={hero} />} />
                     {/* <Route path="*" element={<NotFound />} /> */}
                 </Routes>
--- src/components/Pages/Hunter/Hunter.js
+++ src/components/Pages/Hunter/Hunter.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import '../Paladin/Paladin.css'
+import '../Hunter/Hunter.css'
 
-function Paldin() {
+function Hunter() {
     return (
 
-        <div className='main-paladin-img'>
-            <h1 className='paladin-hero'>Paladin</h1>
+        <div className='main-hunter-img'>
+            <h1 className='hunter-hero'>Paladin</h1>
             <div>
-                <div className='paladin-classInfo'><h1>Class Info</h1>
-                    <div className='paladin-info'>
+                <div className='hunter-classInfo'><h1>Class Info</h1>
+                    <div className='hunter-info'>
                         <p>Paladin talents increase their ability to heal the wounded, strengthen their capacity to protect their companions, and allow them to deal out vicious damage in the name of justice.</p>
                     </div>
                 </div>
-                <div className='paladin-specialization'><h1>Specialization</h1></div>
-                <div className="container-paladin">
-                    <div className="column-paladin">
+                <div className='hunter-specialization'><h1>Specialization</h1></div>
+                <div className="container-hunter">
+                    <div className="column-hunter">
                         <h2>Holy</h2>
                         <p>
                             Invokes the power of the Light to heal and protect allies and vanquish evil from the darkest corners of the world. Preferred Weapon: Sword, Mace, and Shield.
                         </p>
                     </div>
-                    <div className="column-paladin">
+                    <div className="column-hunter">
                         <h2>Retribution</h2>
                         <p>
                             A righteous crusader who judges and punishes opponents with weapons and Holy magic. Preferred Weapon: Two-Handed Sword, Mace, Axe.
                         </p>
                     </div>
-                    <div className="column-paladin">
+                    <div className="column-hunter">
                         <h2>Protection</h2>
                         <p>
                             Uses Holy magic to shield themselves and defend allies from attackers. Preferred Weapon: Sword, Mace, Axe, and Shield.
@@ -39,4 +39,4 @@ function Paldin() {
     );
 }
 
-export default Paldin;
+export default Hunter;
