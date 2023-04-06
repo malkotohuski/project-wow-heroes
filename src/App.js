0165ed4601d780623ab99a2f4a82181c29c9df8d
Daniel Dimitorv <115570079+malkotohuski@users.noreply.github.com>
2023-04-06 22:00:53 +0300

    class-updates

--- src/App.js
+++ src/App.js
@@ -10,20 +10,22 @@ import { Routes, Route, useNavigate, } from 'react-router-dom';
 import Class from './components/Pages/Class';
 import { CreateHero } from './components/Pages/CreateHero';
 import ContactUs from './components/Pages/ContactUs';
-import Marketing from './components/Pages/Marketing';
-import Consulting from './components/Pages/Consulting';
 import { Login } from './components/Pages/Login'
 import { Footer } from './components/Footer';
 import { useState, useEffect, } from 'react';
 import * as HeroService from './services/HeroService';
 import { Catalog } from './components/Catalog/Catalog';
-import Warrior from './components/Pages/Warrior/Warrior';
-import Mage from './components/Pages/Mage/Mage';
-import Druid from './components/Pages/Druid/Druid';
-import Paladin from './components/Pages/Paladin/Paladin';
-import Hunter from './components/Pages/Hunter/Hunter';
-import Priest from './components/Pages/Priest/Priest';
-import Rogue from './components/Pages/Rogue/Rogue';
+import Warrior from './components/HeroClasses/Warrior/Warrior';
+import Mage from './components/HeroClasses/Mage/Mage';
+import Druid from './components/HeroClasses/Druid/Druid';
+import Paladin from './components/HeroClasses/Paladin/Paladin';
+import Hunter from './components/HeroClasses/Hunter/Hunter';
+import Priest from './components/HeroClasses/Priest/Priest';
+import Rogue from './components/HeroClasses/Rogue/Rogue';
+import Shaman from './components/HeroClasses/Shaman/Shaman';
+import { HeroDetails } from './components/Pages/HeroDetails/HeroDetails';
+import Warlock from './components/HeroClasses/Warlock/Warlock';
+import DeathKnight from './components/HeroClasses/DeathKnight/DeathKnight';
 
 
 
@@ -62,8 +64,9 @@ function App() {
                     <Route path='/create-hero' element={<CreateHero onCreateHeroSubmit={onCreateHeroSubmit} />} />
                     <Route path='/contact-us' element={<ContactUs />} />
                     <Route path='/login' element={<Login />} />
-                    <Route path='/marketing' element={<Marketing />} />
-                    <Route path='/consulting' element={<Consulting />} />
+                    <Route path='/catalog' element={<Catalog hero={hero} />} />
+                    <Route path='/catalog/:heroId' element={<HeroDetails />} />
+                    {/* heroes class --> */}
                     <Route path='/class/paladin' element={<Paladin />} />
                     <Route path='/class/warrior' element={<Warrior />} />
                     <Route path='/class/mage' element={<Mage />} />
@@ -71,7 +74,9 @@ function App() {
                     <Route path='/class/hunter' element={<Hunter />} />
                     <Route path='/class/priest' element={<Priest />} />
                     <Route path='/class/rogue' element={<Rogue />} />
-                    <Route path='/catalog' element={<Catalog hero={hero} />} />
+                    <Route path='/class/shaman' element={<Shaman />} />
+                    <Route path='/class/warlock' element={<Warlock />} />
+                    <Route path='/class/death' element={<DeathKnight />} />
                     {/* <Route path="*" element={<NotFound />} /> */}
                 </Routes>
             </main>
