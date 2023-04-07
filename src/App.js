c688ea1fd2079f728ae6895ed381620feb700eb3
Daniel Dimitorv <115570079+malkotohuski@users.noreply.github.com>
2023-04-07 20:50:27 +0300

    login form updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import React from 'react';
-import Navbar from './components/Navbar'
 import './App.css';
 import './Class.css'
 import './components/CreateHero.css'// <-- !!!
-import Home from './components/Pages/Home';
 import { Routes, Route, useNavigate, } from 'react-router-dom';
+import { useState, useEffect, } from 'react';
+import React from 'react';
+
+import Navbar from './components/Navbar'
+import Home from './components/Pages/Home';
 import Class from './components/Pages/Class';
 import { CreateHero } from './components/Pages/CreateHero';
 import ContactUs from './components/Pages/ContactUs';
 import { Login } from './components/Pages/Login'
 import { Footer } from './components/Footer';
-import { useState, useEffect, } from 'react';
 import * as HeroService from './services/HeroService';
 import { Catalog } from './components/Catalog/Catalog';
+import { HeroDetails } from './components/Pages/HeroDetails/HeroDetails';
+import { UserContext } from './context/UserContext';
+
 import Warrior from './components/HeroClasses/Warrior/Warrior';
 import Mage from './components/HeroClasses/Mage/Mage';
 import Druid from './components/HeroClasses/Druid/Druid';
@@ -23,7 +26,6 @@ import Hunter from './components/HeroClasses/Hunter/Hunter';
 import Priest from './components/HeroClasses/Priest/Priest';
 import Rogue from './components/HeroClasses/Rogue/Rogue';
 import Shaman from './components/HeroClasses/Shaman/Shaman';
-import { HeroDetails } from './components/Pages/HeroDetails/HeroDetails';
 import Warlock from './components/HeroClasses/Warlock/Warlock';
 import DeathKnight from './components/HeroClasses/DeathKnight/DeathKnight';
 
@@ -34,6 +36,7 @@ import DeathKnight from './components/HeroClasses/DeathKnight/DeathKnight';
 function App() {
     const navigate = useNavigate()
     const [hero, setHero] = useState([]);
+    const [user, setUser] = useState({});
 
     useEffect(() => {
         HeroService.getAll()
@@ -49,40 +52,46 @@ function App() {
 
         setHero(heroo => [...heroo, newHero]);
 
-      navigate('./catalog') 
+        navigate('./catalog');
     };
 
+    const onLoginSubmit = async (e) => {
+        e.preventDefault()
+       console.log(Object.fromEntries(new FormData(e.target)));
+    }
+
     return (
+        <UserContext.Provider value={{onLoginSubmit}}>
+            <div id='root'>
+                <Navbar />
 
-        <div id='root'>
-            <Navbar />
+                <main in="main-content">
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/class' element={<Class />} />
+                        <Route path='/create-hero' element={<CreateHero onCreateHeroSubmit={onCreateHeroSubmit} />} />
+                        <Route path='/contact-us' element={<ContactUs />} />
+                        <Route path='/login' element={<Login onLoginSubmit={onLoginSubmit} />} />
+                        <Route path='/catalog' element={<Catalog hero={hero} />} />
+                        <Route path='/catalog/:heroId' element={<HeroDetails />} />
+                        {/* heroes class --> */}
+                        <Route path='/class/paladin' element={<Paladin />} />
+                        <Route path='/class/warrior' element={<Warrior />} />
+                        <Route path='/class/mage' element={<Mage />} />
+                        <Route path='/class/druid' element={<Druid />} />
+                        <Route path='/class/hunter' element={<Hunter />} />
+                        <Route path='/class/priest' element={<Priest />} />
+                        <Route path='/class/rogue' element={<Rogue />} />
+                        <Route path='/class/shaman' element={<Shaman />} />
+                        <Route path='/class/warlock' element={<Warlock />} />
+                        <Route path='/class/death' element={<DeathKnight />} />
+                        {/* <Route path="*" element={<NotFound />} /> */}
+                    </Routes>
+                </main>
 
-            <main in="main-content">
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/class' element={<Class />} />
-                    <Route path='/create-hero' element={<CreateHero onCreateHeroSubmit={onCreateHeroSubmit} />} />
-                    <Route path='/contact-us' element={<ContactUs />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/catalog' element={<Catalog hero={hero} />} />
-                    <Route path='/catalog/:heroId' element={<HeroDetails />} />
-                    {/* heroes class --> */}
-                    <Route path='/class/paladin' element={<Paladin />} />
-                    <Route path='/class/warrior' element={<Warrior />} />
-                    <Route path='/class/mage' element={<Mage />} />
-                    <Route path='/class/druid' element={<Druid />} />
-                    <Route path='/class/hunter' element={<Hunter />} />
-                    <Route path='/class/priest' element={<Priest />} />
-                    <Route path='/class/rogue' element={<Rogue />} />
-                    <Route path='/class/shaman' element={<Shaman />} />
-                    <Route path='/class/warlock' element={<Warlock />} />
-                    <Route path='/class/death' element={<DeathKnight />} />
-                    {/* <Route path="*" element={<NotFound />} /> */}
-                </Routes>
-            </main>
-
-            <Footer />
-        </div>
+                <Footer />
+            </div>
+        </UserContext.Provider>
     );
 
 }
---
+++ src/hooks/useForm.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const useForm = (initialValues) => {
+    const [values, setValues] = useState(initialValues);
+
+    const changeHandler = (e) => {
+        setValues(state => ({...state, [e.target.name]: e.target.value}))
+    };
+};
