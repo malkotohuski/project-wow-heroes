19e9b852fc43179febd537232c347caaa32f2f63
Daniel Dimitorv <115570079+malkotohuski@users.noreply.github.com>
2023-04-09 19:14:29 +0300

    login and register update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,103 +1,76 @@
+import { useContext } from 'react';
 import React, { useState } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
 import './Navbar.css';
 
 
 function Navbar() {
-  const [click, setClick] = useState(false);
-  //const [dropdown, setDropdown] = useState(false);
+    const { isUserLogin, userEmail } = useContext(UserContext);
+    const [click, setClick] = useState(false);
+    //const [dropdown, setDropdown] = useState(false);
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
-  /*   const onMouseEnter = () => {
-      if (window.innerWidth < 960) {
-        setDropdown(false);
-      } else {
-        setDropdown(true);
-      }
-    };
-  
-    const onMouseLeave = () => {
-      if (window.innerWidth < 960) {
-        setDropdown(false);
-      } else {
-        setDropdown(false);
-      }
-    }; */
+    /*   const onMouseEnter = () => {
+        if (window.innerWidth < 960) {
+          setDropdown(false);
+        } else {
+          setDropdown(true);
+        }
+      };
+    
+      const onMouseLeave = () => {
+        if (window.innerWidth < 960) {
+          setDropdown(false);
+        } else {
+          setDropdown(false);
+        }
+      }; */
 
-  return (
-    <>
-      <nav className='navibar'>
-        <Link to='/' className='navigation-logo' onClick={closeMobileMenu}>
-          In the eye of the storm
-          <i className="fa fa-desktop" aria-hidden="true"></i>
-        </Link>
-        <div className='navi-icon' onClick={handleClick}>
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-        </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
-            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-              Home
-            </Link>
-          </li>
-          <li
-            className='nav-item'
-          //  onMouseEnter={onMouseEnter}
-          //   onMouseLeave={onMouseLeave}
-          >
-            <Link
-              to='/class'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Hero Class
-            </Link>
+    return (
+        <>
+            <nav className='navibar'>
+                <div className='left-navBar'>
+                    <Link to='/' className='navigation-logo' onClick={closeMobileMenu}>
+                        In the eye of the storm
+                        <i className="fa fa-desktop" aria-hidden="true"></i>
+                    </Link>
+                    <div id="user">
+                        {isUserLogin && (
+                            <div className='nav-item-logout'>
+                                <Link to='/create-hero' className='nav-links-create' onClick={closeMobileMenu}>Create Hero</Link>
+                                <span style={{ color: '#fff' }}>Hello {userEmail}</span>
+                                <div className='nav-item'> <Link to="/logout">Logout</Link></div>
+                            </div>
+                        )}
+                        {!isUserLogin && <li>
+                            <Link to='/login' className='nav-links-mobile' onClick={closeMobileMenu}>Login</Link>
+                        </li>}
+                    </div>
+                </div>
+                <div className='right-navBar'>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/class' className='nav-links' onClick={closeMobileMenu}>Hero Class</Link>
+                        </li>
 
-          </li>
-          <li className='nav-item'>
-            <Link
-              to='/create-hero'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Create Hero
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link
-              to='/contact-us'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Contact Us
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link
-              to='/catalog'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Heroes 
-            </Link>
-          </li>
-          <li>
-            <Link
-              to='/login'
-              className='nav-links-mobile'
-              onClick={closeMobileMenu}
-            >
-              Login
-            </Link>
-          </li>
-        </ul>
-        <Button />
-      </nav>
-    </>
-  );
+                        <li className='nav-item'>
+                            <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>Contact Us</Link>
+                        </li>
+                        <li className='nav-item'><Link to='/catalog' className='nav-links' onClick={closeMobileMenu}>Heroes</Link>
+                        </li>
+                    </ul>
+                    <Button />
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
