73346c17cf13fb43ee739605e282e08524591ef2
Daniel Dimitorv <115570079+malkotohuski@users.noreply.github.com>
2023-04-18 15:06:08 +0300

    css upd class

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -52,45 +52,4 @@ export const Login = () => {
         </div>
     )
 }
-        /* <Fragment>
-<div className="Login-form-container">
-<form className="Login-form" >
-<div className="Login-form-content">
-<h3 className="Login-form-title">Login</h3>
-<div className="text-center">
-Not registered yet?{" "}
-<span className="link-primary" onSubmit={onLoginSubmit}>
-New Registration
-</span>
-</div>
-<div className="form-group mt-3">
-<label>Email address</label>
-<input
-type="email"
-className="form-control mt-1"
-placeholder="Enter email"
-/>
-</div>
-<div className="form-group mt-3">
-<label>Password</label>
-<input
-type="password"
-className="form-control mt-1"
-placeholder="Enter password"
-/>
-</div>
-<div className="d-grid gap-2 mt-3">
-<button type="submit" className="btn btn-primary">
-Submit
-</button>
-</div>
-<p className="text-center mt-2">
-Forgot <Link to="#">password?</Link>
-</p>
-</div>
-</form>
-</div>
-</Fragment>
-)
-}
-*/
+       
